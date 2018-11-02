90f372a7268a53972b404285f2b4e4071d5ce951
hemangi89 <42715532+hemangi89@users.noreply.github.com>
2018-11-02 19:48:08 +0530

    Add files via upload

---
+++ ist.py
@@ -0,0 +1,10 @@
+Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:06:47) [MSC v.1914 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> import os
+>>> os.getcwd()
+'C:\\Users\\Nilam\\AppData\\Local\\Programs\\Python\\Python37-32'
+>>> print(os.getcwd())
+C:\Users\Nilam\AppData\Local\Programs\Python\Python37-32
+>>> os.chdir('C:\Users\Nilam\AppData\Local\Programs\Python\Python37-32\python')
+SyntaxError: (unicode error) 'unicodeescape' codec can't decode bytes in position 2-3: truncated \UXXXXXXXX escape
+>>> 
